d8423935d7466bfc061ffb22ea8d965adad267ae
Mariia <test@example.com>
2026-04-09 21:10:07 +0300

    Apply isort and black formatting

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,38 +1,26 @@
 import pytest
-from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 
 
 @pytest.fixture
 def sample_transactions():
     return [
-        {
-            "id": 1,
-            "description": "Перевод организации",
-            "operationAmount": {"currency": {"code": "USD"}}
-        },
-        {
-            "id": 2,
-            "description": "Перевод со счета на счет",
-            "operationAmount": {"currency": {"code": "RUB"}}
-        },
-        {
-            "id": 3,
-            "description": "Перевод с карты на карту",
-            "operationAmount": {"currency": {"code": "USD"}}
-        },
-        {
-            "id": 4,
-            "description": "Перевод организации",
-            "operationAmount": {"currency": {"code": "EUR"}}
-        }
+        {"id": 1, "description": "Перевод организации", "operationAmount": {"currency": {"code": "USD"}}},
+        {"id": 2, "description": "Перевод со счета на счет", "operationAmount": {"currency": {"code": "RUB"}}},
+        {"id": 3, "description": "Перевод с карты на карту", "operationAmount": {"currency": {"code": "USD"}}},
+        {"id": 4, "description": "Перевод организации", "operationAmount": {"currency": {"code": "EUR"}}},
     ]
 
 
-@pytest.mark.parametrize("currency, first_id, second_id", [
-    ("USD", 1, 3),
-    ("RUB", 2, None),
-    ("EUR", 4, None),
-])
+@pytest.mark.parametrize(
+    "currency, first_id, second_id",
+    [
+        ("USD", 1, 3),
+        ("RUB", 2, None),
+        ("EUR", 4, None),
+    ],
+)
 def test_filter_by_currency(sample_transactions, currency, first_id, second_id):
     gen = filter_by_currency(sample_transactions, currency)
     assert next(gen)["id"] == first_id
@@ -64,10 +52,13 @@ def test_transaction_descriptions_empty():
         next(empty_gen)
 
 
-@pytest.mark.parametrize("start, end, expected", [
-    (1, 3, ["0000 0000 0000 0001", "0000 0000 0000 0002", "0000 0000 0000 0003"]),
-    (10, 10, ["0000 0000 0000 0010"]),
-])
+@pytest.mark.parametrize(
+    "start, end, expected",
+    [
+        (1, 3, ["0000 0000 0000 0001", "0000 0000 0000 0002", "0000 0000 0000 0003"]),
+        (10, 10, ["0000 0000 0000 0010"]),
+    ],
+)
 def test_card_number_generator(start, end, expected):
     gen = card_number_generator(start, end)
     for value in expected:
