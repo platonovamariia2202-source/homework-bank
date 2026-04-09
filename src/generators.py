99207cd4f27c3b023bf81b7421b81ec263fa2327
Mariia <test@example.com>
2026-04-09 18:30:05 +0300

    Add card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -14,3 +14,11 @@ def transaction_descriptions(transactions):
 
     for transaction in transactions:
         yield transaction.get("description", "Описание отсутствует")
+
+def card_number_generator(start: int, end: int):
+    """Генератор номеров карт в заданном диапазоне."""
+
+    for number in range(start, end + 1):
+        num_str = str(number).zfill(16)
+        formatted = " ".join([num_str[i:i+4] for i in range(0, 16, 4)])
+        yield formatted
